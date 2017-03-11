abfc096a10dec397b0cf626d8e284a5007829e4b
Vincent <vinh.nglx@gmail.com>
2017-03-12 00:27:33 +0800

    Rename test method

--- test/test_multiples_3_5.rb
+++ test/test_multiples_3_5.rb
@@ -2,7 +2,7 @@ require 'helper'
 require_relative '../multiples_3_5'
 
 class TestMultiple35 < MiniTest::Test
-  def test_return_zero_if_number_less_than_two_parameters
+  def test_return_sum_of_multiple_numbers_of_3_or_5
     assert_equal 0, Multiple35.sum(1)
     assert_equal 0, Multiple35.sum(2)
     assert_equal 3, Multiple35.sum(3)
