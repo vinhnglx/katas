b4118fe5fd5d48841b2799a7151cd3a400ebb26d
Vincent <vinh.nglx@gmail.com>
2017-03-13 07:44:22 +0800

    ActiveModelSerializers and Rack-cors configurations

---
+++ Ruby/transor/config/initializers/ams.rb
@@ -0,0 +1,10 @@
+ActiveModelSerializers.config.adapter = :json_api
+ActiveModelSerializers.key_transform = :camel_lower
+
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
