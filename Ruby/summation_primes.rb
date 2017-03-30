ad46f4fbcaca348bbba05788c33c7a51f7fdb63c
Vincent <vinh.nglx@gmail.com>
2017-03-31 00:55:26 +0800

    Fix summation primes

--- Ruby/summation_primes.rb
+++ Ruby/summation_primes.rb
@@ -2,6 +2,7 @@ require 'prime'
 
 class SummationPrime
   def self.call(number)
-    (0..number).inject{ |s, x| Prime.prime?(x) ? s + x : s }
+    return 0 if number == 0 || number == 1
+    (2..number).inject{ |s, x| Prime.prime?(x) ? s + x : s }
   end
 end
